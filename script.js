505b6260638895a0cb6ae1285b40b0a80e257d64
Omar2025Noby <109864222+Omar2025Noby@users.noreply.github.com>
2022-08-25 19:56:40 +0300

    الحمد لله عملت شغلي

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
+orders = []
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +8,51 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+let neworder = {name,id,phone,order}
   // أدرج الكائن إلى المصفوفة
-  
+orders.push(neworder)
+
+
+
+
+
+
+
   // طبق الخطوة 7 تحت هذا الكومنت
+  load()
   
 }
 
 function load() {
   let container = document.getElementById("container");
-  container.innerHTML = "";
+
   // طبّق الخطوة 6 تحت هذا الكومنت
+ orders.forEach(item => { container.innerHTML +=  `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3></div>`
+ });
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
